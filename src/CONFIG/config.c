5f17bb084f17dd022d99a03e5298945ba768d8c0
antoine-white <antoine.maxime.bl@gmail.com>
2019-11-29 10:32:05 +0100

    implements some function of config.c

--- src/CONFIG/config.c
+++ src/CONFIG/config.c
@@ -18,35 +18,54 @@
 
 
 // TODO include des .h système
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #include "myassert.h"
 
 #include "config.h"
 
 // TODO Structure de données ici
+typedef struct file_config file_config;
+struct file_config
+{
+    FILE* fp;
+    bool isOpen;
+};
 
+static file_config config = {.fp = NULL, .isOpen = true};
+
+static void assertFileOpen(){
+    myassert(config.isOpen,"operation impossible le fichier de configuration sans avoir appele config_init");
+}
 
 void config_init(const char *filename)
 {
     // TODO erreur si la fonction est appelée deux fois
-
-    // TODO code vide par défaut, à remplacer
+    myassert( ! config.isOpen,"config_init appele une deuxieme fois sans avoir appele config_exit");
+    
+    config.isOpen = true;
+    config.fp = fopen(filename,"r");
+    myassert(config.fp == NULL,"erreur dans l'ouverture du fichier");
 }
 
 void config_exit()
 {
     // TODO erreur si la fonction est appelée avant config_init
+    myassert(config.isOpen,"config_exit appele sans avoir appele config_init");
 
-    // TODO code vide par défaut, à remplacer
+    myassert(fclose(config.fp) == 0,"erreur dans la fermeture du fichier");
 }
 
 int config_getNbServices()
 {
-    // erreur si la fonction est appelée avant config_init
-    // erreur si la fonction est appelée après config_exit
-    
-    // code par défaut, à remplacer
-    return 3;
+    assertFileOpen();    
+
+    rewind(config.fp);// on reviens au début du fichier
+    int temp;
+    fscanf(config.fp,"%d",&temp);
+    return temp;
 }
 
 bool config_isServiceOpen(int pos)
@@ -54,7 +73,7 @@ bool config_isServiceOpen(int pos)
     // TODO erreur si la fonction est appelée avant config_init
     // TODO erreur si la fonction est appelée après config_exit
     // TODO erreur si "pos" est incorrect
-
+    myassert(pos <= config_getNbServices(),"Le service demande a un indice superieur a l'indice du service maximal");
     // TODO code par défaut, à remplacer
     bool open[] = {true, false, true};
     return open[pos-1];
@@ -74,3 +93,4 @@ const char * config_getExeName(int pos)
     };
     return names[pos-1];
 }
+
