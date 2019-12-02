a3fcadfd91a9cd5f4fe49183717ab8965cfb8176
antoine-white <antoine.maxime.bl@gmail.com>
2019-12-02 18:05:47 +0100

    .

--- src/CONFIG/config.c
+++ src/CONFIG/config.c
@@ -34,7 +34,7 @@ struct file_config
     bool isOpen;
 };
 
-static file_config config = {.fp = NULL, .isOpen = true};
+static file_config config = {.fp = NULL, .isOpen = false};
 
 static void assertFileOpen(){
     myassert(config.isOpen,"operation impossible le fichier de configuration sans avoir appele config_init");
@@ -47,7 +47,7 @@ void config_init(const char *filename)
     
     config.isOpen = true;
     config.fp = fopen(filename,"r");
-    myassert(config.fp == NULL,"erreur dans l'ouverture du fichier");
+    myassert(config.fp != NULL,"erreur dans l'ouverture du fichier");
 }
 
 void config_exit()
@@ -72,10 +72,19 @@ bool config_isServiceOpen(int pos)
 {
     // TODO erreur si la fonction est appelée avant config_init
     // TODO erreur si la fonction est appelée après config_exit
+    assertFileOpen();
     // TODO erreur si "pos" est incorrect
-    myassert(pos <= config_getNbServices(),"Le service demande a un indice superieur a l'indice du service maximal");
+    const int nbServiceOpen = config_getNbServices();
+    myassert(pos <= nbServiceOpen,"Le service demande a un indice superieur a l'indice du service maximal");
     // TODO code par défaut, à remplacer
-    bool open[] = {true, false, true};
+    const char open[7] = "ouvert";
+    char line[256];//buffer large 
+    for(int i = 0; i < pos; i++)
+    {
+        fgets(line, sizeof(line), file);
+        
+        open[i] = startsWith(open,line);
+    }
     return open[pos-1];
 }
 
@@ -84,7 +93,7 @@ const char * config_getExeName(int pos)
     // TODO erreur si la fonction est appelée avant config_init
     // TODO erreur si la fonction est appelée après config_exit
     // TODO erreur si "pos" est incorrect
-
+    assertFileOpen();
     // TODO code par défaut, à remplacer
     const char * names[] = {
         "SERVICES/service_somme",
@@ -93,4 +102,16 @@ const char * config_getExeName(int pos)
     };
     return names[pos-1];
 }
+
+//We don't have to worry about str being shorter than pre because according to the C standard (7.21.4.4/2):
+//The strncmp function compares not more than n characters (characters that follow a null character are not compared) from the array pointed to by s1 to the array pointed to by s2."
+//sources : https://stackoverflow.com/questions/4770985/how-to-check-if-a-string-starts-with-another-string-in-c
+static bool startsWith(const char *pre, const char *str)
+{
+    return strncmp(pre, str, strlen(pre)) == 0;
+}
+
+
+
+
 
