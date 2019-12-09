b91c503be1d6e5467318707c20b3dbdeacbe1e29
Med-z <55351063+Med-z@users.noreply.github.com>
2019-12-09 14:19:47 +0100

    Add files via upload

--- src/ORCHESTRE/client_orchestre.c
+++ src/ORCHESTRE/client_orchestre.c
@@ -1,3 +1,100 @@
 #include "myassert.h"
 
 #include "client_orchestre.h"
+
+
+
+
+
+static void createPipe(const char *name, OnePipe *onePipe)
+{
+    int nameLength;
+
+    nameLength = snprintf(NULL, 0, "%s", basename);
+    onePipe->name = malloc((nameLength + 1) * sizeof(char));
+    sprintf(onePipe->name, "%s", basename);
+    onePipe->fd = -1;
+
+    int ret = mkfifo(onePipe->name, 0600);
+}
+
+void o_c_createPipes(Descriptors *pipes)
+{
+    createPipe("pipeOrchestreToClient",O_WRONLY,&(pipes->OtoC));
+    createPipe("pipeClientToOrchestre",O_RDONLY,&(pipes->CtoO));
+}
+
+void c_o_createPipes(Descriptors *pipes)
+{
+    createPipe("pipeOrchestreToClient",O_WRONLY,&(pipes->OtoC));
+    createPipe("pipeClientToOrchestre",O_RDONLY,&(pipes->CtoO));
+}
+
+
+static void destroyPipe(OnePipe *onePipe)
+{
+    int ret;
+    
+    ret = unlink(onePipe->name);
+    myassert(ret == 0, "echec unlink ");
+    free(onePipe->name);
+    onePipe->name = NULL;
+}
+
+void o_destroyPipes(Descriptors *pipes)
+{
+    destroyPipe("destruction tube OtoC", &(pipes->OtoC));
+    destroyPipe("destruction tube CtoO", &(pipes->CtoO));
+}
+
+
+/*
+ * ouverture et fermeture d'un tube
+ */
+ 
+static void openPipe(const char *name, int flag,OnePipe *onePipe)
+{
+    onePipe->name = strdup(name);
+    onePipe->fd = open(onePipe->name, flag);
+    myassert(onePipe->fd != -1, "echec open pipes ");
+}
+
+void c_openPipes(const char *nameCtoO, const char *nameOtoC, Descriptors *pipes)
+{
+    openPipe(nameCtoO, O_WRONLY,
+             "client ouverture tube CtoO", &(pipes->CtoO));
+    openPipe(nameWtoM, O_RDONLY,
+             "orchestre ouverture tube OtoC", &(pipes->OtoC));
+}
+
+void o_openPipes(const char *nameCtoO, const char *nameOtoC, Descriptors *pipes)
+{
+    openPipe(nameCtoO, O_RDONLY,
+             "client ouverture tube CtoO", &(pipes->CtoO));
+    openPipe(nameOtoC, O_WRONLY,
+             "orchestre ouverture tube OtoC", &(pipes->OtoC));
+}
+
+static void closePipe(OnePipe *onePipe)
+{
+    int ret;
+    
+    free(onePipe->name);
+    onePipe->name = NULL;
+    ret = close(onePipe->fd);
+    myassert(ret == 0, "echec fermeture");
+    onePipe->fd = -1;
+}
+
+void c_closePipes(Descriptors *pipes)
+{
+    closePipe("client fermeture tube CtoO", &(pipes->CtoO));
+    closePipe("client fermeture tube OtoC", &(pipes->OtoC));
+}
+
+void o_closePipes(Descriptors *pipes)
+{
+    closePipe("orchestre fermeture tube OtoC", &(pipes->OtoC));
+    closePipe("orchestre fermeture tube CtoO", &(pipes->CtoO));
+}
+
--- src/ORCHESTRE/client_orchestre.h
+++ src/ORCHESTRE/client_orchestre.h
@@ -6,4 +6,58 @@
 //   temps avec l'orchestre
 // - les deux tubes nommés pour la communication bidirectionnelle
 
+
+
+/*
+ * Types et fonctions communes à tous les clients et l'orchestre
+ */
+
+/*
+ * Convention de codage :
+ * - O pour Orchestre
+ * - C pour Client 
+ *
+ * par exemple :
+ * - "nameCtoO" : nom du tube dans le sens client vers orchestre
+ *
+ * Les fonctions côté :
+ * - orchestre sont préfixées par "o_",
+ * - client sont préfixées par "c_",
+ */
+
+/*
+ * Structures pour la paire de descripteurs
+ */
+typedef struct
+{
+    char *name;
+    int fd;
+} OnePipe;
+
+typedef struct {
+    OnePipe CtoO;
+    OnePipe OtoC;
+} Descriptors;
+
+/*
+ * création et destruction des tubes
+ */
+void o_c_createPipes(Descriptors *pipes);
+void c_o_createPipes(Descriptors *pipes);
+void o_destroyPipes(Descriptors *pipes);
+
+/*
+ * ouverture et fermeture des tubes
+ */
+void c_openPipes(const char *nameCtoO, const char *nameOtoC, Descriptors *pipes);
+void o_openPipes(const char *nameCtoO, const char *nameOtoC, Descriptors *pipes);
+
+void c_closePipes(Descriptors *pipes);
+void o_closePipes(Descriptors *pipes);
+
+
+
+
+
+
 #endif
