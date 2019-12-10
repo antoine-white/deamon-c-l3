137d72afcd541c95853737516f37bb062871e282
Med-z <55351063+Med-z@users.noreply.github.com>
2019-12-10 16:32:11 +0100

    function pipes

--- src/SERVICES/client_service.c
+++ src/SERVICES/client_service.c
@@ -1,14 +1,161 @@
-#include <stdio.h> 
-#include <string.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
 #include <fcntl.h> 
 #include <sys/stat.h> 
-#include <sys/types.h> 
-#include <unistd.h> 
-#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
 
 #include "myassert.h"
 
 #include "client_service.h"
+
+static void createPipe(const char *name, PipeClientService *onePipe)
+{
+    int nameLength;
+
+    nameLength = snprintf(NULL, 0, "%s",name);
+    onePipe->name = malloc((nameLength + 1) * sizeof(char));
+    sprintf(onePipe->name, "%s",name);
+    onePipe->fd = -1;
+    
+    int ret = mkfifo(onePipe->name, 0600);
+}
+
+void o_c_createPipes(DescriptorsCS *pipes)
+{
+    createPipe("pipeOrchestreToClient",&(pipes->OtoC));
+    createPipe("pipeClientToOrchestre",&(pipes->CtoO));
+}
+
+void c_o_createPipes(DescriptorsCS *pipes)
+{
+    createPipe("pipeOrchestreToClient",&(pipes->OtoC));
+    createPipe("pipeClientToOrchestre",&(pipes->CtoO));
+}
+
+
+static void destroyPipe(PipeClientService *onePipe)
+{
+    int ret;
+    
+    ret = unlink(onePipe->name);
+    myassert(ret == 0, "echec unlink ");
+    free(onePipe->name);
+    onePipe->name = NULL;
+}
+
+void o_destroyPipes(DescriptorsCS *pipes)
+{
+    destroyPipe(&(pipes->OtoC));
+    destroyPipe(&(pipes->CtoO));
+}
+
+void c_destroyPipes(DescriptorsCS *pipes)
+{
+    destroyPipe(&(pipes->OtoC));
+    destroyPipe(&(pipes->CtoO));
+}
+
+/*
+ * ouverture et fermeture d'un tube
+ */
+ 
+static void openPipe(const char *name, int flag,PipeClientService *onePipe)
+{
+    onePipe->name = strdup(name);
+    onePipe->fd = open(onePipe->name, flag);
+    myassert(onePipe->fd != -1, "echec open pipes ");
+}
+
+void c_openPipes(DescriptorsCS *pipes)
+{
+    openPipe(pipes->CtoO.name, O_WRONLY, &(pipes->CtoO));
+    openPipe(pipes->OtoC.name, O_RDONLY,&(pipes->OtoC));
+}
+
+void o_openPipes(DescriptorsCS *pipes)
+{
+    openPipe(pipes->CtoO.name, O_RDONLY,&(pipes->CtoO)); 
+    openPipe(pipes->OtoC.name, O_WRONLY,&(pipes->OtoC));
+}
+
+static void closePipe(PipeClientService *onePipe)
+{
+    int ret;
+    
+    free(onePipe->name);
+    onePipe->name = NULL;
+    ret = close(onePipe->fd);
+    myassert(ret == 0, "echec fermeture");
+    onePipe->fd = -1;
+}
+
+void c_closePipes(DescriptorsCS *pipes)
+{
+    closePipe(&(pipes->CtoO));
+    closePipe(&(pipes->OtoC));
+}
+
+void o_closePipes(DescriptorsCS *pipes)
+{
+    closePipe(&(pipes->OtoC));
+    closePipe(&(pipes->CtoO));
+}
+
+
+/*
+ * surcharge des envois et réceptions (pour gérer les erreurs)
+ */
+ 
+static void writeData(PipeClientService *onePipe, const void *buf, int size)
+{
+    ssize_t ret = write(onePipe->fd, buf, size);
+    myassert(ret != -1,"echec écriture de données");
+    myassert((size_t)ret == size,"echec écriture de données");
+}
+
+void o_writeData(DescriptorsCS *pipes, const void *buf,int size)
+{
+    writeData(&(pipes->OtoC), buf, size);
+}
+
+void c_writeData(DescriptorsCS *pipes, const void *buf, int size)
+{
+    writeData(&(pipes->CtoO), buf, size);
+}
+
+static void readData(PipeClientService *onePipe, void *buf,int size)
+{
+    ssize_t ret = read(onePipe->fd, buf, size);
+    myassert(ret != -1,"echec lecture de données");
+    //myassert((size_t)ret == size, "echec lecture de données");
+}
+
+void o_readData(DescriptorsCS *pipes, void *buf, int size)
+{
+    readData(&(pipes->CtoO), buf, size);
+
+}
+
+void c_readData(DescriptorsCS *pipes, void *buf, int size)
+{
+    readData(&(pipes->OtoC), buf, size);
+}
+
+
+
+
+
+
+
+
+
+
+
 
  int getPwdFromClient(int fifoFd)
  {
@@ -36,3 +183,9 @@ bool clientAcknowledges(int fifoFd)
     read(fifoFd,&msg,sizeof(char) * 100);
     return strstr(msg, "RESULT OK") != NULL;// on vérifie qu'il y a la chaîne RESULT OK dans msg
 }
+
+
+/*******************************************************/
+
+
+
--- src/SERVICES/client_service.h
+++ src/SERVICES/client_service.h
@@ -5,6 +5,63 @@
 
 // Ici toutes les communications entre les services et les clients :
 // - les deux tubes nommés pour la communication bidirectionnelle
+
+/*
+ * Convention de codage :
+ * - S pour Service
+ * - C pour Client 
+ *
+ * par exemple :
+ * - "nameCtoS" : nom du tube dans le sens client vers service
+ *
+ * Les fonctions côté :
+ * - service sont préfixées par "s_",
+ * - client sont préfixées par "c_",
+ */
+
+/*
+ * Structures pour la paire de descripteurs
+ */
+typedef struct
+{
+    char *name;
+    int fd;
+} PipeClientService;
+
+typedef struct {
+    PipeClientService CtoO;
+    PipeClientService OtoC;
+} DescriptorsCS;
+
+/*
+ * création et destruction des tubes
+ */
+void s_c_createPipes(DescriptorsCS *pipes);
+void c_s_createPipes(DescriptorsCS *pipes);
+void s_destroyPipes(DescriptorsCS *pipes);
+void c_destroyPipes(DescriptorsCS *pipes);
+
+/*
+ * ouverture et fermeture des tubes
+ */
+void c_openPipes(DescriptorsCS *pipes);
+void s_openPipes(DescriptorsCS *pipes);
+
+void c_closePipes(DescriptorsCS *pipes);
+void s_closePipes(DescriptorsCS *pipes);
+
+
+/*
+ * surcharge des envois et réceptions (pour gérer les erreurs)
+ */
+void s_writeData(DescriptorsCS *pipes, const void *buf, int size);
+void c_writeData(DescriptorsCS *pipes, const void *buf,int size);
+void s_readData(DescriptorsCS *pipes, void *buf, int size);
+void c_readData(DescriptorsCS *pipes, void *buf, int size);
+
+
+
+
 int getPwdFromClient(int fifoFd);
 void sendErrorPwd(int fifoFd);
 void sendOkPwd(int fifoFd);
